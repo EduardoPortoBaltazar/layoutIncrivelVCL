6d1fea5cb0eb8e3e008dfe9119d1d93911676e94
eduardoportobaltazar <eduardo.porto.baltazar@gmail.com>
2021-12-08 21:45:26 -0300

    Projeto Inicial Layout Incrivel

---
+++ Project/LayoutIncrivel.dpr
@@ -0,0 +1,14 @@
+program LayoutIncrivel;
+
+uses
+  Vcl.Forms,
+  FPrincipal in 'FPrincipal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ Source/FPrincipal.pas
@@ -0,0 +1,30 @@
+unit FPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.jpeg, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Image1: TImage;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Panel5: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
